e15b5d9865ed6013c1892f6ce70b2b3ff9b6a37c
AnubhavPundir <51020539+AnubhavPundir@users.noreply.github.com>
2024-06-09 01:09:19 +0530

    Response headers for CORS error fix

---
+++ src/main/java/com/weatherapiproject/weatherapi/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.weatherapiproject.weatherapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("https://anubhavpundir.github.io", "https://anubhavpundir.github.io/city_weather_frontend/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                .allowCredentials(true);
+    }
+}
